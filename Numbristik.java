a161c3395717b84353a7114ceff735c9bd61d6cc
OlenKarin <karin.kruup16@gmail.com>
2015-03-11 22:43:13 +0200

    arendus. Kood on veel katki aga juba läheb!

--- Numbristik.java
+++ Numbristik.java
@@ -4,79 +4,79 @@ import sun.font.TrueTypeFont;
  * Created by Karin on 9.03.2015.
  */
 
-public class Numbristik extends Peaklass {
+/*public class Numbristik extends Peaklass {
     /*public static void main(String[] args) {
         kontrollEsimene("m", 3);
         System.out.println(kontrollTeine("m",3));
     }*/
-    public static void kontrollEsimene(String taht, int arv) {
-            if (taht.equals("n")) {
-                if (nul.getT().contains(taht) && arv == nul.getN()) {
-                    System.out.println("Õige!");
-                } else {
-                    System.out.println("Vale! Õige on " + nul.getT());
-                }
-            } else if (taht.equals("l")) {
-                if (one.getT().contains(taht) && arv == one.getN()) {
-                    System.out.println("Õige!");
-
-                } else {
-                    System.out.println("Vale! Õige on "+one.getT());
-                }
-            } else if (taht.equals("k") || taht.equals("g")) {
-                if (two.getT().contains(taht) && arv == two.getN()) {
-                    System.out.println("Õige!");
-                } else {
-                    System.out.println("Vale! Õige on " + two.getT());
-                }
-            } else if (taht.equals("m")) {
-                if (three.getT().contains(taht) && arv == three.getN()) {
-                    System.out.println("Õige!");
-                } else {
-                    System.out.println("Vale! Õige on " + three.getT());
-                }
-            }else if (taht.equals("t")) {
-                if (four.getT().contains(taht) && arv == four.getN()) {
-                    System.out.println("Õige!");
-                } else {
-                    System.out.println("Vale! Õige on " + four.getT());
-                }
-            }else if (taht.equals("v")||taht.equals("w")|| taht.equals("f")) {
-                if (five.getT().contains(taht) && arv == five.getN()) {
-                    System.out.println("Õige!");
-                } else {
-                    System.out.println("Vale! Õige on " + five.getT());
-                }
-            }else if (taht.equals("b")||taht.equals("p")) {
-                if (six.getT().contains(taht) && arv == six.getN()) {
-                    System.out.println("Õige!");
-                } else {
-                    System.out.println("Vale! Õige on " + six.getT());
-                }
-            }else if (taht.equals("s")) {
-                if (seven.getT().contains(taht) && arv == seven.getN()) {
-                    System.out.println("Õige!");
-                } else {
-                    System.out.println("Vale! Õige on " + seven.getT());
-                }
-            }else if (taht.equals("r")) {
-                if (eight.getT().contains(taht) && arv == eight.getN()) {
-                    System.out.println("Õige!");
-                } else {
-                    System.out.println("Vale! Õige on " + eight.getT());
-                }
-            }else if (taht.equals("j")||taht.equals("h")||taht.equals("d")) {
-                if (nine.getT().contains(taht) && arv == nine.getN()) {
-                    System.out.println("Õige!");
-                } else {
-                    System.out.println("Vale! Õige on " + nine.getT());
-                }
-            }else{
-                System.out.println("Sisestasid sootuks vale tähe!");
+   /* public static void kontrollEsimene(String taht, int arv) {
+        if (taht.equals("n")) {
+            if (nul.getT().contains(taht) && arv == nul.getN()) {
+                System.out.println("Õige!");
+            } else {
+                System.out.println("Vale! Õige on " + nul.getT());
             }
+        } else if (taht.equals("l")) {
+            if (one.getT().contains(taht) && arv == one.getN()) {
+                System.out.println("Õige!");
 
+            } else {
+                System.out.println("Vale! Õige on "+one.getT());
+            }
+        } else if (taht.equals("k") || taht.equals("g")) {
+            if (two.getT().contains(taht) && arv == two.getN()) {
+                System.out.println("Õige!");
+            } else {
+                System.out.println("Vale! Õige on " + two.getT());
+            }
+        } else if (taht.equals("m")) {
+            if (three.getT().contains(taht) && arv == three.getN()) {
+                System.out.println("Õige!");
+            } else {
+                System.out.println("Vale! Õige on " + three.getT());
+            }
+        }else if (taht.equals("t")) {
+            if (four.getT().contains(taht) && arv == four.getN()) {
+                System.out.println("Õige!");
+            } else {
+                System.out.println("Vale! Õige on " + four.getT());
+            }
+        }else if (taht.equals("v")||taht.equals("w")|| taht.equals("f")) {
+            if (five.getT().contains(taht) && arv == five.getN()) {
+                System.out.println("Õige!");
+            } else {
+                System.out.println("Vale! Õige on " + five.getT());
+            }
+        }else if (taht.equals("b")||taht.equals("p")) {
+            if (six.getT().contains(taht) && arv == six.getN()) {
+                System.out.println("Õige!");
+            } else {
+                System.out.println("Vale! Õige on " + six.getT());
+            }
+        }else if (taht.equals("s")) {
+            if (seven.getT().contains(taht) && arv == seven.getN()) {
+                System.out.println("Õige!");
+            } else {
+                System.out.println("Vale! Õige on " + seven.getT());
+            }
+        }else if (taht.equals("r")) {
+            if (eight.getT().contains(taht) && arv == eight.getN()) {
+                System.out.println("Õige!");
+            } else {
+                System.out.println("Vale! Õige on " + eight.getT());
+            }
+        }else if (taht.equals("j")||taht.equals("h")||taht.equals("d")) {
+            if (nine.getT().contains(taht) && arv == nine.getN()) {
+                System.out.println("Õige!");
+            } else {
+                System.out.println("Vale! Õige on " + nine.getT());
+            }
+        }else{
+            System.out.println("Sisestasid sootuks vale tähe!");
         }
-    public static  boolean kontrollTeine(String taht, int arv) {
+
+    }
+    /*public static  boolean kontrollTeine(String taht, int arv) {
         if (taht.equals("n")) {
             if (nul.getT().contains(taht) && arv == nul.getN()) {
                 return true;
@@ -142,4 +142,4 @@ public class Numbristik extends Peaklass {
             return false;
         }
     }
-}
+}*/
